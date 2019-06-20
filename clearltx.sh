8d97410aaec65018bf47bb343fa649e405b1161b
Golubitskiy Savva <golubitskiy_savv@mail.ru>
2019-06-20 11:27:03 +0300

    vkr

---
+++ clearltx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+find -name "*.fdb*" -delete 
+find -name "*.aux"  -delete
+find -name "*.log"  -delete
+find -name "*.fls"  -delete
+find -name "*.gz"  -delete
+find -name "*.bbl"  -delete
+find -name "*.blg"  -delete
+find -name "*.toc"  -delete
+find -name "*.out"  -delete
